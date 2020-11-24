3ba559d677bd9062de9ecf42f70031f51d8d0160
leahzheng <zan272905918@163.com>
2020-11-24 22:52:48 +0800

    renderList

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,21 @@ import './imports';
 
 import Header from '../components/Header/index';
 import NavBar from '../components/NavBar/index';
+import NewsList from '../components/NewsList/index';
+import PageLoading from '../components/PageLoading/index';
 
 import {NEWS_TYPE} from '../data/index';
 import serviecs from '../serviecs';
 ;((doc)=>{
     const oApp = doc.querySelector('#app');
+    let oListWrapper = null;
+
     const config = {
         type:'top',
-        count:10
+        count:10,
+        pageNum:0
     };
-    const newsData = [];
+    const newsData = {};
     
     
      const init = async()=>{
@@ -33,25 +38,42 @@ import serviecs from '../serviecs';
         });
 
         const navBar = NavBar.tpl(NEWS_TYPE)
+        const listWrapperTpl = NewsList.wrapperTpl(82);
+        oApp.innerHTML += (headerTpl + navBar + listWrapperTpl);
+        oListWrapper = oApp.querySelector('.news-list')
+    }
 
-        oApp.innerHTML += (headerTpl + navBar);
+    function renderList(data){
+        const {pageNum } = config;
+        const newsListTpl = NewsList.tpl({
+            data,pageNum
+        });
+        oListWrapper.innerHTML += newsListTpl;
+        NewsList.imgShow();
     }
 
     function setType(type){
         config.type = type;
-        console.log(config.type);
+        config.pageNum = 0;
+        oListWrapper.innerHTML = '';
+        getNewsList()
     }
 
     async function getNewsList(){
-        const {type, count} = config;
-
+        const {type, count, pageNum} = config;
+        //如果缓存池里有数据
         if (newsData[type]){
-
+            
+            renderList(newsData[type][pageNum]);
             return;
         }
-
+        //缓存池里没有数据
+        oListWrapper.innerHTML = PageLoading.tpl();
         newsData[type] = await serviecs.getNewsList(type,count);
-        console.log(newsData);
+        setTimeout(()=>{
+            oListWrapper.innerHTML = '';
+            renderList(newsData[type][pageNum]);
+        },1000);
     }
     
     init();
