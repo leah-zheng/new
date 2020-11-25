a85a5d87b325c9d4ab8648aa5df1026114216879
leahzheng <zan272905918@163.com>
2020-11-25 10:29:16 +0800

    moreLoading

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,21 @@ import Header from '../components/Header/index';
 import NavBar from '../components/NavBar/index';
 import NewsList from '../components/NewsList/index';
 import PageLoading from '../components/PageLoading/index';
+import MoreLoading from '../components/MoreLoading/index';
 
 import {NEWS_TYPE} from '../data/index';
 import serviecs from '../serviecs';
+import { scrollToBottom } from '../libs/utils';
 ;((doc)=>{
     const oApp = doc.querySelector('#app');
     let oListWrapper = null;
+    let t = null;
 
-    const config = {
+    let config = {
         type:'top',
         count:10,
-        pageNum:0
+        pageNum:0,
+        isLoading:false
     };
     const newsData = {};
     
@@ -26,7 +30,8 @@ import serviecs from '../serviecs';
     }
 
     function bindEvent(){
-        NavBar.bindEvent(setType)
+        NavBar.bindEvent(setType);
+        window.addEventListener('scroll',scrollToBottom.bind(null,getMoreList) ,false);
     }
 
     function render(){
@@ -40,7 +45,8 @@ import serviecs from '../serviecs';
         const navBar = NavBar.tpl(NEWS_TYPE)
         const listWrapperTpl = NewsList.wrapperTpl(82);
         oApp.innerHTML += (headerTpl + navBar + listWrapperTpl);
-        oListWrapper = oApp.querySelector('.news-list')
+        oListWrapper = oApp.querySelector('.news-list');
+
     }
 
     function renderList(data){
@@ -48,15 +54,18 @@ import serviecs from '../serviecs';
         const newsListTpl = NewsList.tpl({
             data,pageNum
         });
+        MoreLoading.remove(oListWrapper);
         oListWrapper.innerHTML += newsListTpl;
+        config.isLoading = false;
         NewsList.imgShow();
     }
 
     function setType(type){
         config.type = type;
         config.pageNum = 0;
+        config.isLoading = false;
         oListWrapper.innerHTML = '';
-        getNewsList()
+        getNewsList();
     }
 
     async function getNewsList(){
@@ -66,15 +75,36 @@ import serviecs from '../serviecs';
             
             renderList(newsData[type][pageNum]);
             return;
+        }else{
+            //缓存池里没有数据
+            oListWrapper.innerHTML = PageLoading.tpl();
+            newsData[type] = await serviecs.getNewsList(type,count);
+            setTimeout(()=>{
+                oListWrapper.innerHTML = '';
+                renderList(newsData[type][pageNum]);
+            },1000);    
         }
-        //缓存池里没有数据
-        oListWrapper.innerHTML = PageLoading.tpl();
-        newsData[type] = await serviecs.getNewsList(type,count);
-        setTimeout(()=>{
-            oListWrapper.innerHTML = '';
-            renderList(newsData[type][pageNum]);
-        },1000);
+        
+        
+        
     }
+
+    function getMoreList () {
+        if (!config.isLoading) {
+          config.pageNum ++;
+          clearTimeout(t);
+          const { pageNum, type } = config;
+          if (pageNum >= newsData[type].length) {
+            MoreLoading.add(oListWrapper, false);
+          } else {
+            config.isLoading = true;
+            MoreLoading.add(oListWrapper, true);
+            t = setTimeout(() => {
+              getNewsList();
+            }, 2000);
+          }
+        }
+      }
     
     init();
 })(document)
