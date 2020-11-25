8de31efec67a7aea6b54e726c0231ab5c363d0b2
leahzheng <zan272905918@163.com>
2020-11-25 22:58:37 +0800

    collections

--- src/js/collections.js
+++ src/js/collections.js
@@ -1,14 +1,19 @@
 import './imports';
 
 import Header from '../components/Header/index';
+import NewsList from '../components/NewsList/index';
+import NoDataTip from '../components/NoDataTip/index';
 
 import {tplReplace} from '../libs/utils';
 
 ;((doc)=>{
     const oApp = doc.querySelector('#app');
+    const followedList = JSON.parse(localStorage.getItem('followedList')) || [];
+    let oListWrapper = null;
 
     const init = ()=>{
-        render()
+        render();
+        bindEvent();
     }
 
     function render(){
@@ -17,9 +22,36 @@ import {tplReplace} from '../libs/utils';
             title:'我的收藏',
             showLeftIcon:true,
             showRightIcon:false
+        });
+
+        if(followedList.length){
+            const listWrapperTpl = NewsList.wrapperTpl(44);
+            oApp.innerHTML += (collectionsTpl + listWrapperTpl);
+            oListWrapper = oApp.querySelector('.news-list');
+            renderList(followedList);
+            NewsList.imgShow();
+        } else{
+            oApp.innerHTML += (collectionsTpl+NoDataTip.tpl())
+        }
+    }
+
+    function renderList(data){
+        const newsListTpl = NewsList.tpl({
+            data,
+            pageNum:-1
         })
 
-        oApp.innerHTML += collectionsTpl;
+        oListWrapper.innerHTML += newsListTpl;
+    }
+
+    function bindEvent(){
+        followedList.length && NewsList.bindEvent(oListWrapper,setCurrentNews)
+    }
+
+    function setCurrentNews(options){
+        const {idx} = options;
+        const currentNews = followedList[idx];
+        localStorage.setItem('currentNews',JSON.stringify(currentNews));
     }
 
     init()
--- src/js/detail.js
+++ src/js/detail.js
@@ -2,12 +2,15 @@ import './imports';
 import Header from '../components/Header/index';
 import { getUrlQueryValue } from '../libs/utils';
 import NewsFrame from '../components/Iframe/index';
+import Follow from '../components/Follow/index';
 
 ;((doc)=>{
     const oApp = doc.querySelector('#app');
-    const currentNews = JSON.parse(localStorage.getItem('currentNews'))
+    const currentNews = JSON.parse(localStorage.getItem('currentNews'));
+    const followedList = JSON.parse(localStorage.getItem('followedList') || '[]')
     const init = ()=>{
-        render()
+        render();
+        bindEvent();
     }
 
     function render(){
@@ -20,9 +23,38 @@ import NewsFrame from '../components/Iframe/index';
 
         const newsFrameTpl = NewsFrame.tpl(
             currentNews.url
-        )
+        );
 
-        oApp.innerHTML += (collectionsTpl+ newsFrameTpl);
+        const followTpl = createFolloeTpl();
+
+        oApp.innerHTML += (collectionsTpl+ newsFrameTpl + followTpl);
+    }
+
+    function createFolloeTpl(){
+        //followedList里是否有当前新闻的uniquekey
+        const isExist = followedList.find(item =>{
+            return item.uniquekey === currentNews.uniquekey
+        })
+
+        return isExist ? Follow.follow() : Follow.unfollow();
+    }
+
+    function bindEvent(){
+        Follow.bindEvent(doFollow);
+    }
+
+    function doFollow(status){
+        let followedList = JSON.parse(localStorage.getItem('followedList'))||[] ;
+
+        if(status){
+            followedList.push(currentNews);
+        }else{
+            followedList = followedList.filter(item =>{
+                return item.uniquekey !== currentNews.uniquekey
+            })
+        }
+
+        localStorage.setItem('followedList',JSON.stringify(followedList))
     }
 
     init()
